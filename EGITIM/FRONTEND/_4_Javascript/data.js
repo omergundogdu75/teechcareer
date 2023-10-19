c210ae49e5b8cd9ba39cdcd2dbd286df9165e940
ÖMER GÜNDOĞDU <omergundogdu75@gmail.com>
2023-10-19 10:41:51 +0300

    JS kodları

---
+++ EGITIM/FRONTEND/_4_Javascript/data.js
@@ -0,0 +1,129 @@
+//window.console.log("loglama")
+// console.log("Loglama")
+// console.warn("Warn log")
+// console.error("Error log")
+
+// //Variable
+// var global = 44;
+// console.log(global);
+
+// var global = 44;
+// console.log(global);
+
+// var global = 44;
+// console.log(global);
+
+// //ES(2016)
+// //let
+// let localVariable=23;
+// console.log(localVariable);
+
+// localVariable=42;
+// console.log(localVariable);
+
+// //const
+// const localVariable2 = "Merhaba";
+// console.log(localVariable2);
+
+// //Common Rules
+// //üşğçö
+// let dataVariable44 = 44;
+// console.log(dataVariable44);
+
+// let $_dataVariable55 = 55;
+// console.log($_dataVariable55)
+// //--------------------------------
+// // + - * / %
+// //x++ ++x
+// //x-- --x
+// // &&=ve ||=veya  !
+// let data10 = "Merhabalar";
+// console.log(typeof data10)
+
+// //hoisting
+// data11 = 1>2;
+// var data11;
+// console.log(data11)
+
+//let data12 = prompt("Lütfen kullanıcı adını giriniz: ");
+//console.log(data12)
+
+// console.log(Math.min(2,1,-100,500));
+// console.log(Math.max(2,1,-100,500));
+// console.log(Math.sin(-1));
+// console.log(Math.sqrt(25));
+// console.log(Math.abs(-25));
+// console.log(Math.pow(2,5));
+// console.log(Math.floor(2.9));
+// console.log(Math.floor(2.9));
+// console.log(Math.round(6.4));
+// console.log(Math.round(6.5));
+// console.log(Math.ceil(4.4));
+// console.log(Math.ceil(4.5));
+
+//let data12 = Math.abs(Number(prompt("Lütfen bbir sayı giriniz")));
+//let data12 = Number(prompt("Lütfen bbir sayı giriniz"));
+//let data13 = Math.floor((Math.random()*data12+1).toFixed(2));
+//console.log(data13)
+
+
+//----------------------------
+let data14 = " js ÖğrEniyorum js";
+console.log(data14);
+console.log(data14.length);
+console.log(data14.trim.length)
+console.log(data14.startsWith(" "))
+console.log(data14.endsWith("s"))
+console.log(data14.toLowerCase())
+console.log(data14.toUpperCase())
+
+data14 = data14.concat(" -inc");
+console.log(data14);
+
+let data = " js ÖğrEniyorum js";
+data14 = data14.replace(data14,data);
+console.log(data14);
+
+console.log(data14.charAt(1));
+console.log(data14.substring(0,4));
+
+console.log(data14.indexOf("js"));
+console.log(data14.lastIndexOf("js"));
+
+// ÖDEV
+
+// ÖDEV // kullanıcı tarafından girilen bir kelimenin (prompt)
+// let userData= prompt("Lütfen bir şeyler yazınız")  ;
+// S-1) Kaç karakterlidir ? (length)
+// S-2) boşluklar alınarak Kaç karakterlidir ? (trim)
+// S-3) bütün kelimeyi küçük harfle göstermek ? (toLowerCase())
+// S-4) bütün kelimeyi büyük harfle göstermek ? (toUpperCase());
+// S-5) Kullanıcıdan alınan ilk kelimeyle , "javascript" kelimeyle değiştirin. (replace, substring, indexOf)
+// console.log(userData.replace("xxxxxxx"),"javascript");
+// console.log(userData.replace(userData.substring(0,userData.indexOf(" ")),"javascript"));
+// S-6) yazdığınız kelimede "javascript ile mi başlıyor" ? (startsWith()) 
+// S-7) yazdığınız kelimede "javascript ile mi bitiyor" ? (endsWith())
+// S-8) ilk harf nedir  ? (charAt())
+// S-9) girdiğiniz kelimenin sonuna "-ben js öğreniyorum" (concat())
+// S-10) girdiğiniz 0 ile 4 arasındaki indexi ekranda gösterin? (subString())
+
+
+/////////////////////////////////////////////////////////////////////////////
+// Function
+
+// normal Function 
+// anonymous function
+// arrow function
+// immedia function
+
+
+
+
+
+
+
+
+
+
+
+
